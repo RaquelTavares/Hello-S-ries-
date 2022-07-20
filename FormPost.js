d53a27af877436dba79275199f99d85c6e98e8e8
RaquelTavares <98133747+RaquelTavares@users.noreply.github.com>
2022-07-20 19:42:35 -0300

    Add files via upload

---
+++ FormPost.js
@@ -0,0 +1,11 @@
+// export class FormPost{
+//     constructor(teste) {
+//         this.teste = teste
+//         this.console();
+//     }
+//     console(){
+//         return console.log('esta é a mensagem: ${this.teste}')
+//     }
+
+// }
+// const postForm = New FormPost('Bacana esta mensagem')
